f7c2c99a6f2113558655483558c2ab74a2344c2b
Kelly Hurt <Kelly.Hurt@tptlive.ee>
2024-11-15 11:39:42 +0200

    Repository

--- KooliProjekt/Data/Artist.cs
+++ KooliProjekt/Data/Artist.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 
 namespace KooliProjekt.Data
 {
-    public class Artist
+    public class Artist : Entity
     {
-        [Required]
-        public int Id { get; set; }
-
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
---
+++ KooliProjekt/Data/Repository/IArtistRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace KooliProjekt.Data.Repository
+{
+    public interface IArtistRepository
+    {
+        Task<Artist> Get(int id);
+        Task<PagedResult<Artist>> List(int page, int pageSize);
+    }
+}
