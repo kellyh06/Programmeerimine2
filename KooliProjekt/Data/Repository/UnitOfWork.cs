9152cf7010085a1f9bce56a72a7be0498d51e03e
kellyh06 <kelly.hurt@tptlive.ee>
2025-01-30 14:08:20 +0200

    Repo unitest

---
+++ KooliProjekt/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using KooliProjekt.Data.Repository;
+
+namespace KooliProjekt.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context,
+            IArtistRepository todoItemRepository,
+            IArtistRepository todoListRepository)
+        {
+            _context = context;
+
+            TodoItemRepository = todoItemRepository;
+            TodoListRepository = todoListRepository;
+        }
+
+        public IArtistRepository TodoItemRepository { get; private set; }
+        public IArtistRepository TodoListRepository { get; private set; }
+
+        public async Task BeginTransaction()
+        {
+            await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task Commit()
+        {
+            await _context.Database.CommitTransactionAsync();
+        }
+
+        public async Task Rollback()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+    }
+}
